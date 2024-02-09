504f2558f58445014f809258e6a6622ad19d7cab
Vaibhav sarda <158248067+vaibhavsunilsarda37@users.noreply.github.com>
2024-02-09 09:32:35 +0530

    Add files via upload

---
+++ Script intro.js
@@ -0,0 +1,6 @@
+document.getElementById('changeButton').addEventListener('click', function() {
+    // Simulate making changes
+    alert('Made changes!');
+
+    // In a real scenario, you'd perform actual changes and commits here.
+});
